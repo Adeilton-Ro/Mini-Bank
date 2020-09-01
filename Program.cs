9d2dec8e5ebbe339e8eef45cbd032f7a555641e5
Adeilton <adeilton@pop-os.localdomain>
2020-09-01 16:49:34 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("Adeilton",1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            BankAccount a = new BankAccount("Marcos", 1000);
+            Console.WriteLine(a.GetAccountHistory());
+        }
+    }
+}
